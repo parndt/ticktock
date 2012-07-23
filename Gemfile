e7e25527322bbddf1a5073f4681755621c0e5bc1
Philip Arndt <parndt@gmail.com>
2012-07-23 17:21:47 +1200

    Added activesupport and support for semimonthly period hours reports.

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source :rubygems
 gem 'harvested'
+gem 'activesupport'
--- ticktock.rb
+++ ticktock.rb
@@ -1,18 +1,46 @@
 require 'bundler/setup'
 require 'pathname'
+require 'yaml'
 require 'harvested'
+require 'active_support/core_ext'
 
 class TickTock
 
   attr_accessor :config
 
-  def initialize
-    unless (config_file = Pathname.new(File.expand_path('../config/authentication.yml', __FILE__))).exist?
-      FileUtils::cp "#{config_file}.example", config_file.to_s
+  def initialize(config = File.expand_path('../config/authentication.yml', __FILE__))
+    read_config! config
+  end
+
+  def report!(user_id = @config[:user_id], from, to)
+    harvest.reports.time_by_user user_id, from, to
+  end
+
+  def semimonthly_hours!(user_id = @config[:user_id])
+    entries = report! user_id, *current_semimonthly_period
+    entries.map { |entry| entry.hours.to_f }.sum
+  end
+
+  def harvest
+    @harvest ||= Harvest.client config[:subdomain],
+                                config[:username],
+                                config[:password]
+  end
+
+  def current_semimonthly_period
+    @timeframe = if Time.now.day <= 15
+      [Time.now.beginning_of_month, Time.now.beginning_of_month + 14.days]
+    else
+      [Time.now.beginning_of_month + 15.days, Time.now.end_of_month]
     end
-    @config = YAML::load config_file.read
+  end
+
+  def read_config!(config)
+    unless File.exist?(config)
+      FileUtils::cp "#{config}.example", config.to_s
+    end
+    @config = ::HashWithIndifferentAccess.new
+    @config.update YAML::load(File.read(config))
   end
 
 end
-
-TickTock.new
