cd341a75d84fe0f5f5087dccb47221e19d853c4b
Philip Arndt <parndt@gmail.com>
2012-07-23 16:44:07 +1200

    Let's read some configuration.

---
+++ ticktock.rb
@@ -0,0 +1,17 @@
+require 'bundler/setup'
+require 'harvested'
+
+class TickTock
+
+  attr_accessor :config
+
+  def initialize
+    unless (config_file = Pathname.new(File.expand_path('../config/authentication.yml', __FILE__))).exist?
+      FileUtils::cp "#{config_file}.example", config_file.to_s
+    end
+    @config = YAML::load config_file.read
+  end
+
+end
+
+TickTock.new
